a876880f06d642e73a516ca18b2f0f3a39f8ed07
Aleksi Cohen <alcohen@student.hive.fi>
2020-01-16 19:59:51 +0200

    Flags

--- ft_printf/flags.c
+++ ft_printf/flags.c
@@ -6,7 +6,7 @@
 /*   By: alcohen <alcohen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 16:59:19 by alcohen           #+#    #+#             */
-/*   Updated: 2020/01/16 19:44:56 by alcohen          ###   ########.fr       */
+/*   Updated: 2020/01/16 19:59:24 by alcohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,15 @@ char	*get_flag(const char *format, int i)
 {
 	size_t	len;
 
-	len = 0;
-	while (format[i])
-	{
-		if (ft_strchr("cspdiouxXf%", format[i]));
+	len = 1;
+	while (format[i + len]) {
+		if (ft_strchr("cspdiouxXf%", format[i + len]))
 		{
-			return (ft_strsub((char const *)format, i, len));
+			return (ft_strsub((char const *)format, i + 1, len));
 		}
 		len++;
-		i++;
 	}
-
+	return (NULL);
 }
 
 int		handle_flags(char *flag, va_list list_ptr)
